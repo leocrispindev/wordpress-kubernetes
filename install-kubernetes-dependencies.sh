8443d8f9b7106063dc74b02ffd395867588c43f9
lcrispin <leonardo.crispin.oliveira00@gmail.com>
2024-09-15 12:42:23 -0300

    shell scrpt para instalar o ambiente

---
+++ install-kubernetes-dependencies.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Baixando e instalando K3s"
+curl -sfL https://get.k3s.io | sh -
+
+echo "Mudando permissão de arquivos de configuração do ambiente K3s"
+sudo chmod 644 /etc/rancher/k3s/k3s.yaml
+
+echo "Executando arquivo Kustomize - aplicando configurações do kubernetes"
+kubectl apply -k .
+
